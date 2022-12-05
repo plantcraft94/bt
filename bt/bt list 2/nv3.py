8bc4ae66221c6b97f7cf173e1e43a89dca9445d9
plantcraft94 <plantcraft60@gmail.com>
2022-12-05 22:13:23 +0700

    update nvu 3 | 5/12/2022

--- bt/bt list 2/nv3.py
+++ bt/bt list 2/nv3.py
@@ -1,16 +1,8 @@
-A = [4,5,6,1,2,3,7,8,9,10]
-
-#quan trọng (nếu nó đúng :) )
-for i in A:
-    if i == 1:
-        a = A.index(i)
-        if A[a] == 1 and A[a+1] == 2 and A[a+2] == 3:
-            print(f"vị trí bắt đầu của dãy số 1,2,3 là {a}")
-            break
-        else:
-            print("Không tìm thấy mẫu")
-            break
-
-    elif i == A[-1]:
-        print("Không tìm thấy mẫu")
-#chắc đúng đấy :) bài khó vl
+A=[0,4,0,1,2,3,8,9,0,1,2,3,17,-16,0,1,2]
+co = False
+for i in range (len(A)-2):                              #
+    if A[i]==1 and A[i+1]==2 and A[i+2]==3:             #  Cre: Đình Đức
+        print(f"dãy số 1,2,3 xuất hiện ở vị trí {i}")   #
+        co = True
+if co == False:
+    print("Không tìm thấy mẫu")
